ad7df7f18310679e83b357dfed2fb755629a5f6c
Saim-Ahmed007 <saimahmed.seu.cse@gmail.com>
2025-06-28 22:48:54 +0600

    add some changes

--- movie-ticket-booking-server/inngest/index.js
+++ movie-ticket-booking-server/inngest/index.js
@@ -7,20 +7,28 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 //Inngest functions to save user into the database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async({event}) => {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id: id,
-            name: first_name + ' ' + last_name,
-            email: email_addresses[0],
-            image: image_url
-        }
-        await User.create(userData)
-         return { ok: true };
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+      const userData = {
+        _id: id,
+        name: `${first_name} ${last_name}`,
+        email: email_addresses[0]?.email_address || "",
+        image: image_url
+      };
+
+      await User.create(userData);
+
+      return { ok: true };
+    } catch (error) {
+      console.error("Failed to sync user from Clerk:", error);
+      return { ok: false, error: error.message };
     }
-)
+  }
+);
 
 //Inngest functions to delete user into the database
 const syncUserDeletion = inngest.createFunction(
